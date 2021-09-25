44ac12093fbaf3a212fd27a22b11c0d8034d8384
LDonGithub <83192358+LDonGithub@users.noreply.github.com>
2021-09-24 23:20:04 -0300

    Agregado Split + Tipos de retorno

--- ServicioSoap/servicio.php
+++ ServicioSoap/servicio.php
@@ -7,7 +7,23 @@ $ns = "urn:miserviciowsdl";
 $servicio->configureWSDL("ServicioWebSoap", $ns);
 $servicio->schemaTargetNamespace = $ns;
 
+$servicio->wsdl->addComplexType(
+    'Nombres',
+    'complexType',
+    'array',
+    'sequence',
+    '',
+    array(
+      'Nombre' => array(
+        'name' => 'Nombre', 
+        'type' => 'xsd:string',
+        'minOccurs' => '0', 
+        'maxOccurs' => 'unbounded'
+      )
+    )
+  );
 $servicio->register("ValDigVer", array('RutSinDig' => 'xsd:integer', 'DigVer' => 'xsd:integer'), array('return' => 'xsd:boolean'), $ns);
+$servicio->register("Split", array('Nom' => 'xsd:string'), array('nombres' => 'tns:Nombres', 'apellidos' => 'tns:Nombres' ), $ns);
 
 function ValDigVer($RutSinDig, $DigVer){
     $s=1;
@@ -20,6 +36,25 @@ function ValDigVer($RutSinDig, $DigVer){
     }else{
         return false;
     }
+}
+
+function Split($Nom){
+    $Nombres = array();
+    $Apellidos = array('a','a');
+    
+    $aux = explode(" ",$Nom);
+    
+    $Apellidos[1] = array_pop($aux);
+    $Apellidos[0] = array_pop($aux);
+
+    $lengt = count($aux);
+
+    for($i=0;$i<$lengt;$i++)
+        $Nombres[$i]=$aux[$i];
+
+    $estructura = ["Nombres" => $Nombres , "Apellido_1" => $Apellidos[0],"Apellido_2" => $Apellidos[1] ];
+
+    return [["los","pol","kol"],["asd","poil","kojl"]];
 }
 
 $servicio->service(file_get_contents("php://input"));
