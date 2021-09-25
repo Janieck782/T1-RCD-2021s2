e6f0e103023361507a38db2bf55e8a51cf62b9f8
Rafael Morales <rmorales@utem.cl>
2021-09-25 18:10:47 -0300

    Limpiando codigo

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,8 @@
 (async function () {
   const api = await fetch("../config.json")
-    .then((response) => response.json())
-    .then(
-      (config) =>
-        `http://${config.api_host}:${config.api_port}/${config.location}/api/`
-    );
+    .then(response => response.json())
+    .then(config => `http://${config.api_host}:${config.api_port}/${config.location}/api/` );
 
-  console.log(api);
   fetch(api + "separar/rafael morales venegas")
     .then((response) => response.json())
     .then((data) => {
