53968722b8d81a80869bf953e28da80ecfe3a3b5
Rafael Morales <rmorales@utem.cl>
2021-09-25 16:57:57 -0300

    Obteniendo resultados de la API

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,15 @@
 (async function () {
   const api = await fetch("../config.json")
     .then((response) => response.json())
-    .then((config) => `http://${config.api_host}:${config.api_port}/${config.location}/api/`);
+    .then(
+      (config) =>
+        `http://${config.api_host}:${config.api_port}/${config.location}/api/`
+    );
 
-    console.log(api);
-    fetch(api + 'separar/rafael morales venegas')//.then(data => { console.log(data); });
-
+  console.log(api);
+  fetch(api + "separar/rafael morales venegas")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 })();
