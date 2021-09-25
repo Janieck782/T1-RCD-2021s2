a47871ced8f8cae1957521baf24b260edb9e4607
Rafael Morales <rmorales@utem.cl>
2021-09-25 19:02:12 -0300

    Agregada pantalla de error en caso de 404 del API

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,9 @@
 (async function () {
   const api = await fetch("../config.json")
     .then(response => response.json())
-    .then(config => `http://${config.api_host}:${config.api_port}/${config.location}/api/` );
+    .then(config => `http://${config.api_host}:${config.api_port}/${config.location}/api` );
 
-  fetch(api + "separar/rafael morales venegas")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+  fetch(api).catch(error => {
+        document.querySelector("#api_error").removeAttribute('hidden');
     });
 })();
