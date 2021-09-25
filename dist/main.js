d6b6876b40105a07617313759a57006d7568baf9
Rafael Morales <rmorales@utem.cl>
2021-09-25 19:37:12 -0300

    Primera llamada a la API lista

--- dist/main.js
+++ dist/main.js
@@ -6,27 +6,39 @@
   fetch(api).catch(error => {
         document.querySelector("#api_error").removeAttribute('hidden');
     });
-})();
-
-(function () {
-  'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('.needs-validation');
 
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
-        event.preventDefault()
+        event.preventDefault();
         if (!form.checkValidity()) {
-          event.stopPropagation()
+          event.stopPropagation();
+        }
+        else if(form.id == "separar_nombres") {
+            const nombre = form.querySelector('#nombre_input').value;
+            fetch(api + "/separar/" + nombre)
+            .then(response => response.json())
+            .then(data => { console.log(data); });
+        }
+        else if(form.id == "validar_rut"){
+
         }
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-function validar(){
-    
+        form.classList.add('was-validated');
+
+      }, false);
+    });
+})();
+
+function separarnombres(element){
+    const string = element.value;
+    const array = string.split(' ');
+    if(array.length < 3){
+        return element.setCustomValidity("Se necesitan al menos 3 palabras")
+    }
+    return element.setCustomValidity("");
 }
